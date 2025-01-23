61f433aa2710db05b695e4f4528f8329f2e5106f
Ulico <aruss8@illinois.edu>
2025-01-22 22:49:06 -0600

    Reformated files into folder

--- cogs/showdown_commands.py
+++ cogs/showdown_commands.py
@@ -12,8 +12,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 import utils
-import players
-import replay_analyzer
+import misc_programs.players
+import misc_programs.replay_analyzer as replay_analyzer
 
 
 class UploadReplayButton(discord.ui.Button):
--- test.py
+++
@@ -1,3 +0,0 @@
-import replay_analyzer
-
-print(replay_analyzer.get_ots_data("Ulico"))
